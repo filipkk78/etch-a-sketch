3a0552a51866b60aa021c399e90ed8a37daa79c5
filipkk78 <filipkk78@gmail.com>
2024-07-10 18:39:14 +0200

    Change color of grid squares to blue

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-
 for (let i = 0; i<256; i++) {
     const container = document.querySelector("#container");
     
     const square = document.createElement("div");
     square.classList.add("square");
-    container.appendChild(square);
+    container.appendChild(square);    
 }
+

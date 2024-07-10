932e7de3074eeb8af36239892192a1eec3e5cf68
filipkk78 <filipkk78@gmail.com>
2024-07-10 18:11:54 +0200

    Add a 16x16 grid

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+for (let i = 0; i<256; i++) {
+    const container = document.querySelector("#container");
+    
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+}

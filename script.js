7d68685427a21c086ae86ea79f65fe8f2ea78543
filipkk78 <filipkk78@gmail.com>
2024-07-10 20:27:29 +0200

    Add changing number of squares

--- script.js
+++ script.js
@@ -11,5 +11,28 @@ container.addEventListener("mouseover",(event) => { event.target.style.backgroun
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function () {
     let canvasWidth = prompt("Enter the canvas width");
-    let canvasHeight = prompt("Enter the canvas height")
+    let canvasHeight = prompt("Enter the canvas height");
+    generateCanvas(canvasWidth, canvasHeight);
 });
+
+let squareWidth;
+let squareHeight;
+let squareCount;
+
+function generateCanvas(canvasX, canvasY) {
+    squareWidth = (100/canvasX);
+    squareHeight = (100/canvasY); 
+    for (let i = 0; i<256; i++) {
+        const square = document.querySelector("div");
+        container.removeChild(container.lastChild);   
+    }
+    container.style.backgroundColor="";
+    squareCount = canvasX*canvasY;
+    for (let i = 0; i<squareCount; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.flexBasis=`${squareWidth}%`;
+        square.style.height=`${squareHeight}%`;
+        container.appendChild(square);    
+    }
+}

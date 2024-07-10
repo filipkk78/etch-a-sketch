3cff807823d95aa98a5bf486f04d3a0f5a634350
filipkk78 <filipkk78@gmail.com>
2024-07-11 01:11:55 +0200

    Modify script to prevent non squares

--- script.js
+++ script.js
@@ -8,32 +8,30 @@ for (let i = 0; i<256; i++) {
 
 container.addEventListener("mouseover",(event) => { event.target.style.backgroundColor = "purple"}, false)
 
-let canvasWidth
-let canvasHeight
+let canvasDimensions;
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function () {
     do {
-    alert("These two numbers mustn't exceed 100 in order to prevent crashes.");
-    canvasWidth = prompt("Enter the canvas width:");
-    canvasHeight = prompt("Enter the canvas height:"); 
-    } while (canvasHeight > 101 && canvasWidth > 101 || canvasHeight > 101 || canvasWidth > 101);
-    generateCanvas(canvasWidth, canvasHeight);
+    alert("This number mustn't exceed 100 in order to prevent crashes.");
+    canvasDimensions = prompt("Enter the canvas width:");
+    } while (canvasDimensions > 100);
+    generateCanvas(canvasDimensions);
 });
 
 let squareWidth;
 let squareHeight;
 let squareCount;
 
-function generateCanvas(canvasX, canvasY) {
-    squareWidth = (100/canvasX);
-    squareHeight = (100/canvasY); 
+function generateCanvas(canvasXY) {
+    squareWidth = (100/canvasXY);
+    squareHeight = (100/canvasXY); 
     for (let i = 0; i<256; i++) {
         const square = document.querySelector("div");
         container.removeChild(container.lastChild);   
     }
     container.style.backgroundColor="";
-    squareCount = canvasX*canvasY;
+    squareCount = (canvasXY**2);
     for (let i = 0; i<squareCount; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
